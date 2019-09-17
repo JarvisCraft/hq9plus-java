c68dccaca29ac41fae2b5fc250bb75d888b5a00e
PROgrammer_JARvis <mrjarviscraft@gmail.com>
2019-09-17 23:32:19 +0300

    Make `HQ9PlusCompiler` throw exceptions

--- hq9plus-jvm-compiler/src/main/java/ru/progrm_jarvis/lang/hq9plus/HQ9PlusCompiler.java
+++ hq9plus-jvm-compiler/src/main/java/ru/progrm_jarvis/lang/hq9plus/HQ9PlusCompiler.java
@@ -1,8 +1,10 @@
 package ru.progrm_jarvis.lang.hq9plus;
 
 import lombok.NonNull;
+import lombok.SneakyThrows;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.IOException;
 import java.util.function.BiConsumer;
 
 /**
@@ -18,10 +20,13 @@ public interface HQ9PlusCompiler<I, O> extends BiConsumer<I, O> {
      *
      * @param input input source providing compilation source (normally, source code)
      * @param output output target receiving compilation result
+     *
+     * @throws IOException if an exception occurs while reading or writing data
      */
-    void compile(@NonNull I input, @NotNull O output);
+    void compile(@NonNull I input, @NotNull O output) throws IOException;
 
     @Override
+    @SneakyThrows
     default void accept(@NotNull final I input, @NotNull final O output) {
         compile(input, output);
     }

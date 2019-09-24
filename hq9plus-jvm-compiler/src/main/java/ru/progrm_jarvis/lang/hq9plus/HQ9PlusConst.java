41be0a4f2dec9b3f5bc978facb373c9901eaeb96
PROgrammer_JARvis <mrjarviscraft@gmail.com>
2019-09-24 23:56:47 +0300

    Add `DEFAULT_BEER_BOTTLE_COUNT` constant

--- hq9plus-jvm-compiler/src/main/java/ru/progrm_jarvis/lang/hq9plus/HQ9PlusConst.java
+++ hq9plus-jvm-compiler/src/main/java/ru/progrm_jarvis/lang/hq9plus/HQ9PlusConst.java
@@ -12,4 +12,9 @@ public class HQ9PlusConst {
      * Hello world text
      */
     public String HELLO_WORLD_TEXT = "Hello, world!";
+
+    /**
+     * Default amount of bottles of beer meant by {@link ru.progrm_jarvis.lang.hq9plus.ast.HQ9PlusAstNode#NINE}
+     */
+    public final int DEFAULT_BEER_BOTTLE_COUNT = 99;
 }

522621affb661acca200fb8f9a8c0b3a8e8ae7ef
PROgrammer_JARvis <mrjarviscraft@gmail.com>
2019-09-24 21:54:47 +0300

    Specialize `HQ9PlusCompiler` more accurately

--- hq9plus-jvm-compiler/src/main/java/ru/progrm_jarvis/lang/hq9plus/HQ9PlusCompiler.java
+++ hq9plus-jvm-compiler/src/main/java/ru/progrm_jarvis/lang/hq9plus/HQ9PlusCompiler.java
@@ -1,11 +1,9 @@
 package ru.progrm_jarvis.lang.hq9plus;
 
 import lombok.NonNull;
-import lombok.SneakyThrows;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
-import java.util.function.BiConsumer;
 
 /**
  * Compiler for <b>HQ9+ programming language</b>.
@@ -13,21 +11,17 @@ import java.util.function.BiConsumer;
  * @param <I> type of input source providing compilation source (normally, source code)
  * @param <O> type of output target receiving compilation result
  */
-public interface HQ9PlusCompiler<I, O> extends BiConsumer<I, O> {
+public interface HQ9PlusCompiler<I, O> {
 
     /**
      * Compiles the source from the input passing the result into output.
      *
+     * @param className name of the compiled class
      * @param input input source providing compilation source (normally, source code)
      * @param output output target receiving compilation result
      *
      * @throws IOException if an exception occurs while reading or writing data
+     * @throws ru.progrm_jarvis.lang.hq9plus.ast.HQ9PlusAstParseException if an exception occurs while parsing AST
      */
-    void compile(@NonNull I input, @NotNull O output) throws IOException;
-
-    @Override
-    @SneakyThrows
-    default void accept(@NotNull final I input, @NotNull final O output) {
-        compile(input, output);
-    }
+    void compile(@NonNull String className, @NonNull I input, @NotNull O output) throws IOException;
 }

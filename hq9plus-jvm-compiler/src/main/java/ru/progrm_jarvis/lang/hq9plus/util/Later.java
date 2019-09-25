4707d7f318cd123b3ca4be4a56145dc2bffd3096
PROgrammer_JARvis <mrjarviscraft@gmail.com>
2019-09-25 23:29:17 +0300

    Add `Later` (mostly for internal usage)

---
+++ hq9plus-jvm-compiler/src/main/java/ru/progrm_jarvis/lang/hq9plus/util/Later.java
@@ -0,0 +1,36 @@
+package ru.progrm_jarvis.lang.hq9plus.util;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Value;
+import lombok.experimental.FieldDefaults;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+/**
+ * Value used to combine the value and a function.
+ *
+ * @param <C> type of already computed value
+ * @param <P> type of the second parameter needed for the delayed computation
+ * @param <L> type of the value computed later
+ */
+@Value(staticConstructor = "of")
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public final class Later<C, P, L> implements Function<P, L> {
+
+    /**
+     * The value already computed
+     */
+    @NonNull C computedValue;
+    /**
+     * Function used for performing computations depending the computed value
+     */
+    @NonNull @Getter(AccessLevel.NONE) BiFunction<C, P, L> computer;
+
+    @Override
+    public L apply(final P parameter) {
+        return computer.apply(computedValue, parameter);
+    }
+}

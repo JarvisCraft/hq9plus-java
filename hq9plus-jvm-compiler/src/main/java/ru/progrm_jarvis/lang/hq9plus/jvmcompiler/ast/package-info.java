d5269439952de5942403694749c4fad07cb4cfe8
PROgrammer_JARvis <mrjarviscraft@gmail.com>
2019-09-26 19:28:25 +0300

    Add initial `package-info`s

---
+++ hq9plus-jvm-compiler/src/main/java/ru/progrm_jarvis/lang/hq9plus/jvmcompiler/ast/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * AST (abstract-syntax-tree) related objects of <b>HQ9+ programming language</b>
+ */
+package ru.progrm_jarvis.lang.hq9plus.jvmcompiler.ast;
---
+++ hq9plus-jvm-compiler/src/main/java/ru/progrm_jarvis/lang/hq9plus/jvmcompiler/compiler/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Basic abstraction and implementations of <b>HQ9+ programming language</b> compilers
+ */
+package ru.progrm_jarvis.lang.hq9plus.jvmcompiler.compiler;
